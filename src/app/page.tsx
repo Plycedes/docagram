16fc02e502df9b825a035b70bfacf36b9f9d34d8
Plycedes <abhaypratapyadav340@gmail.com>
2025-02-17 23:11:54 +0530

    Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
     return (
@@ -6,14 +8,31 @@ export default function Home() {
             <section className="remove-scrollbar container my-auto">
                 <div className="sub-container max-w-[496px]">
                     <Image
-                        src="../../assets/icons/logo-full.svg"
+                        src="/assets/icons/logo-full.svg"
                         height={1000}
                         width={1000}
                         alt="patient"
                         className="mb-12 h-10 w-fit"
                     />
+                    <PatientForm />
+                    <div className="text-14-regular mt-20 flex justify-between">
+                        <p className="justify-items-end text-dark-600 xl:text-left">
+                            © 2020 Docagram
+                        </p>
+                        <Link href="/?admin=true" className="text-green-500">
+                            Admin
+                        </Link>
+                    </div>
                 </div>
             </section>
+
+            <Image
+                src="/assets/images/onboarding-img.png"
+                height={1000}
+                width={1000}
+                alt="patient"
+                className="side-img max-w-[50%]"
+            />
         </div>
     );
 }
