c997460ad5f6fbad6677d7938d96d49b47e5f646
Plycedes <abhaypratapyadav340@gmail.com>
2025-02-18 16:46:27 +0530

    FormField

---
+++ src/components/CustomFormField.tsx
@@ -0,0 +1,45 @@
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control } from "react-hook-form";
+import { FormFieldType } from "./forms/PatientForm";
+
+interface CustomProps {
+    fieldType: FormFieldType;
+    control: Control<any>;
+    name: string;
+    label?: string;
+    placeholder?: string;
+    iconSrc?: string;
+    iconAlt?: string;
+    disabled?: boolean;
+    dateFormat?: string;
+    showTimeSelect?: boolean;
+    children?: React.ReactNode;
+    renderSkeleton?: (field: any) => React.ReactNode;
+}
+
+const CustomFormField = ({ fieldType, control, name, label }: CustomProps) => {
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <FormItem className="flex-1">
+                    {fieldType !== FormFieldType.CHECKBOX && label && (
+                        <FormLabel>{label}</FormLabel>
+                    )}
+                </FormItem>
+            )}
+        />
+    );
+};
+
+export default CustomFormField;
