876d4f502c76ad7ad5910435fbe98ae0539a313f
Plycedes <abhaypratapyadav340@gmail.com>
2025-02-23 22:19:49 +0530

    Get User

--- src/app/patients/[userId]/register/page.tsx
+++ src/app/patients/[userId]/register/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 
 import RegisterForm from "@/components/forms/RegisterForm";
+import { getUser } from "@/lib/actions/patient.actions";
 
-const Register = () => {
+const Register = async ({ params: { userId } }: SearchParamProps) => {
+    const user = await getUser(userId);
+
     return (
         <div className="flex h-screen max-h-screen">
             <section className="remove-scrollbar container my-auto">
